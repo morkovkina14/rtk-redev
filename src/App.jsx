f5119017ae7d521be9cf75da0fb8bec10edea15f
Olga Morkovkina <ola1999ola1999@mail.ru>
2026-09-17 13:17:06 +0100

    дз8-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { MainContent, Section } from "./components/Content";
-
+import React from "react";
+import ParentComponent from "./components/ParentComponent";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <ParentComponent />
     </div>
   );
 };
